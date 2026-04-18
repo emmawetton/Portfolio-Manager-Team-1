62067edf9963dcdda5dd4d015109b5a2ea57af56
Emma Wetton <emma@wetton.net>
2026-04-18 11:58:14 +0000

    temporary removal of projct application tests

--- PortfolioManager/src/test/java/com/portfolio/api/PortfolioManagerApplicationTests.java
+++ PortfolioManager/src/test/java/com/portfolio/api/PortfolioManagerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.portfolio.api;
+// package com.portfolio.api;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PortfolioManagerApplicationTests {
+// @SpringBootTest
+// class PortfolioManagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
